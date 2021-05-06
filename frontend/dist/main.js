592da92f42a1bdb805194412b85e7366f8364115
Claudia Sinowato <claudia.sinowato@gmail.com>
2021-05-06 15:44:55 -0400

    Voice chat credentials

--- frontend/dist/main.js
+++ frontend/dist/main.js
@@ -113,8 +113,8 @@ exports.createPresignedS3URL = function (name, options) {
 
 exports.createPresignedURL = function (method, host, path, service, payload, options) {
   options = options || {};
-  options.key = options.key || process.env.AWS_ACCESS_KEY_ID;
-  options.secret = options.secret || process.env.AWS_SECRET_ACCESS_KEY;
+  options.key = "AKIAXTO7IHCVX3UCLKLO" //options.key || process.env.AWS_ACCESS_KEY_ID;
+  options.secret = "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" //options.secret || process.env.AWS_SECRET_ACCESS_KEY;
   options.protocol = options.protocol || 'https';
   options.headers = options.headers || {};
   options.timestamp = options.timestamp || Date.now();
@@ -214,7 +214,7 @@ $('#start-button').click(function () {
   window.navigator.mediaDevices.getUserMedia({
     video: false,
     audio: true
-  }) // ...then we convert the mic stream to binary event stream messages when the promise resolves 
+  }) // ...then we convert the mic stream to binary event stream messages when the promise resolves
   .then(streamAudioToWebSocket)["catch"](function (error) {
     showError('There was an error streaming your audio to Amazon Transcribe. Please try again.');
     toggleStartStop();
@@ -36375,13 +36375,13 @@ Script.prototype.runInContext = function (context) {
     if (!(context instanceof Context)) {
         throw new TypeError("needs a 'context' argument.");
     }
-    
+
     var iframe = document.createElement('iframe');
     if (!iframe.style) iframe.style = {};
     iframe.style.display = 'none';
-    
+
     document.body.appendChild(iframe);
-    
+
     var win = iframe.contentWindow;
     var wEval = win.eval, wExecScript = win.execScript;
 
@@ -36390,7 +36390,7 @@ Script.prototype.runInContext = function (context) {
         wExecScript.call(win, 'null');
         wEval = win.eval;
     }
-    
+
     forEach(Object_keys(context), function (key) {
         win[key] = context[key];
     });
@@ -36399,11 +36399,11 @@ Script.prototype.runInContext = function (context) {
             win[key] = context[key];
         }
     });
-    
+
     var winKeys = Object_keys(win);
 
     var res = wEval.call(win, this.code);
-    
+
     forEach(Object_keys(win), function (key) {
         // Avoid copying circular objects like `top` and `window` by only
         // updating existing context properties or new properties in the `win`
@@ -36418,9 +36418,9 @@ Script.prototype.runInContext = function (context) {
             defineProp(context, key, win[key]);
         }
     });
-    
+
     document.body.removeChild(iframe);
-    
+
     return res;
 };
 
