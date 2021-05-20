ea415a0becb5aa3e9e9a6a47d3b95656d314d849
Jin Gyu Lee <ccjglingm07@gmail.com>
2021-05-20 10:40:22 -0400

    update recipe js

--- frontend/js/recipejs.js
+++ frontend/js/recipejs.js
@@ -117,13 +117,13 @@ function saveReciepe() {
     console.log('Saving recipe')
 
     console.log('recipeID: ' + id)
-    console.log('userName: ' + cognitoUser)
+    console.log('user: ' + cognitoUser)
 
     let params = {
         "Content-Type": "multipart/form-data"
     }
     let body = {
-        "username": username,
+        "username": cognitoUser,
         "recipeID": id
     }
     let additionalParams = {}
@@ -139,69 +139,3 @@ function saveReciepe() {
             alert("Something went Wrong!")
         })
 }
-
-// // //something similar to below
-// function uploadNewRecipe(e){
-//     e.preventDefault();
-//     console.log('Uploading new recipe')
-
-//     var title = document.getElementById("add-recipe-form").elements[0].value;
-//     let imageurl = document.getElementById("add-recipe-form").elements[1].value;
-//     let ingredients = document.getElementById("add-recipe-form").elements[2].value;
-//     let instructions = document.getElementById("add-recipe-form").elements[3].value;
-
-//     console.log("FORM DETAILS", title, imageurl, ingredients, instructions)
-
-//     // Get user data from Cognito
-//     let data = {
-//         UserPoolId: config.cognito.userPoolId,
-//         ClientId: config.cognito.clientId
-//     };
-//     console.log("DATA", data);
-
-//     let CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
-//     let userPool =  new AmazonCognitoIdentity.CognitoUserPool(data);
-//     let cognitoUser = userPool.getCurrentUser();
-
-//     let username;
-//     if (cognitoUser) {
-//         username = cognitoUser.username
-//     } else {
-//         username = ''
-//     }
-
-//     console.log("USER POOL", userPool)
-//     console.log("cognito user", cognitoUser)
-//     console.log("username", username)
-
-
-//     // Uploading recipe via API /PUT method
-//     async function uploadRecipePUT() {
-//         e.preventDefault();
-//         console.log("Upload button clicked...");
-
-//         // Connect to API Gateway
-//         let apigClient = apigClientFactory.newClient();
-//         console.log("apigClient", apigClient);
-
-//         let params = {
-//             "Content-Type": "multipart/form-data"
-//         };
-//         let body = {
-//             "username": username,
-//             "title": title,
-//             "imageurl": imageurl,
-//             "ingredients": ingredients,
-//             "instructions": instructions,
-//         };
-//         let additionalParams = {};
-
-//         // API GATEWAY
-//         apigClient.uploadPut(params, body, additionalParams);
-
-//         // Clear form after submit
-//         $("#add-recipe-form")[0].reset();
-//     }
-
-//     uploadRecipePUT()
-// }
